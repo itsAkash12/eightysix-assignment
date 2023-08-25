b76bf3169a03c80d65af0635e789ecbf6b5199be
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-08-25 23:32:48 +0530

    added: controllers for the posts to perform operations

---
+++ backend/controllers/postControllers.js
@@ -0,0 +1,110 @@
+const PostModel = require("../models/PostModel");
+const UserModel = require("../models/UserModel");
+
+// Creating a new post
+const createPost = async (req, res) => {
+  try {
+    const { user_id, content } = req.body;
+    const userExists = await UserModel.findOne({ _id: user_id });
+    if (!userExists) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const newPost = await PostModel.create({ user_id, content });
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Retrieving a post by id
+const getPostById = async (req, res) => {
+  try {
+    const post = await PostModel.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found." });
+    }
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Updating a post's content by id
+const updatePost = async (req, res) => {
+  try {
+    const updatedPost = await PostModel.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found." });
+    }
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Deleting a post by id
+const deletePost = async (req, res) => {
+  try {
+    const deletedPost = await PostModel.findByIdAndDelete(req.params.id);
+    if (!deletedPost) {
+      return res.status(404).json({ message: "Post not found." });
+    }
+    res.json({ message: "Post deleted." });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Incrementing the like count of a post by id
+const likePost = async (req, res) => {
+  try {
+    const updatedPost = await PostModel.findByIdAndUpdate(
+      req.params.id,
+      { $inc: { likes: 1 } },
+      { new: true }
+    );
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found." });
+    }
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Decrementing the like count of a post by id
+const unlikePost = async (req, res) => {
+  try {
+    const post = await PostModel.findOne({ _id: req.params.id });
+    if (post.likes > 0) {
+      const updatedPost = await PostModel.findByIdAndUpdate(
+        req.params.id,
+        { $inc: { likes: -1 }},
+        { new: true }
+      );
+      if (!updatedPost) {
+        return res.status(404).json({ message: "Post not found." });
+      }
+      res.json(updatedPost);
+    }else{
+      return res.status(404).json({message:"Likes can't decrement after 0"})
+    }
+    
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createPost,
+  getPostById,
+  updatePost,
+  deletePost,
+  likePost,
+  unlikePost,
+};
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const postRoute = express.Router();
+const {createPost, getPostById, updatePost, deletePost, likePost, unlikePost} = require("../controllers/postControllers")
 
 postRoute.post('/', createPost);
 postRoute.get('/:id', getPostById);
-postRoute.put('/:id', updatePost);
+postRoute.patch('/:id', updatePost);
 postRoute.delete('/:id', deletePost);
 postRoute.post('/:id/like', likePost);
 postRoute.post('/:id/unlike', unlikePost);
